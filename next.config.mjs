b19b7c5dfc60aebe269e6b0fe7ac6a00901dd9f2
Keno Dressel <hire@keno.digital>
2024-11-14 17:48:39 +0100

    adjusts output folder
    (cherry picked from commit db32b2aa9b06504a7716fbe18da5d8f150364ca0)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  distDir: "./build", // Changes the build output directory to `./dist`.
 };
 
 export default nextConfig;
